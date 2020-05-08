f477db4e5c36655bbee4d2c75c19fc0add7dd2fc
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-05-08 10:34:44 +0200

    Removed created_at and modified_at fields

--- tests/Fixture/CategoriesFixture.php
+++ tests/Fixture/CategoriesFixture.php
@@ -17,8 +17,6 @@ class CategoriesFixture extends TestFixture
     public $fields = [
         'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
         'cat_name' => ['type' => 'string', 'length' => 45, 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
-        'created_at' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
-        'modified_at' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
         ],
@@ -39,8 +37,6 @@ class CategoriesFixture extends TestFixture
             [
                 'id' => 1,
                 'cat_name' => 'Lorem ipsum dolor sit amet',
-                'created_at' => '2020-04-28 21:09:48',
-                'modified_at' => '2020-04-28 21:09:48',
             ],
         ];
         parent::init();
